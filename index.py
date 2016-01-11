4094a74f6ad8c7242f9c9c4422cbb5231bd884d8
iiiiiwan <dirty_brier@yahoo.co.jp>
2016-01-11 20:09:11 +0900

    === FIRST COMMIT ===

---
+++ index.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*- 
+
+### Hello World
+
+# Hello World [1]
+print 'Hello world! [1]'
+
+# Hello World [2]
+print 'Hello world! [2]'
+
+# -----------------------------------------------------------
